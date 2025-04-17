cab0616da6efaab83ee4c31440d5fa5b8ac4b828
ibro-pro <ibrahimabdullahiibrahim150@gmail.com>
2025-04-17 16:45:54 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <P>Iam doing labs</P>
+    <img src={myImage} alt="I made this" />
+  </div>;
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,12 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  
+  return <div>
+
+  </div>;
+ 
+  
 }
 
 export default Home;
